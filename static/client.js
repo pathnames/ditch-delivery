02d0e9d8301c212aea07089e341cb95fb159a58b
pjkpjk900 <pk58@buffalo.edu>
2020-07-04 17:08:44 -0400

    Everything except search works

--- static/client.js
+++ static/client.js
@@ -5,6 +5,8 @@ var r_15 = ['Taco Salad', 'Chicken Pasta', 'The real reuben sandwich', 'Healthy 
 var r_5 = ['Tofu Banh Mi', 'Sandwiches for lunch', 'Breakfast sandwiches', 'Green goddess sandwiches', 'Toasted pan bagnat', 'Veg grilled sandwiches', 'Tomato cucumber sandwich', 'Russian tea sandwiches'];
 var recipes = r_5.concat(r_15, r_25);
 
+
+
 var random_urls = ["https://www.delish.com/cooking/recipe-ideas/recipes/a55762/honey-garlic-glazed-salmon-recipe/", "https://cleananddelicious.com/easy-sandwich-recipes-for-school-work/", "http://foodfitnessfreshair.com/2015/12/06/tofu-banh-mi/", "https://lizoncall.com/taco-salad-15minutesuppers/", "https://gimmedelicious.com/quick-easy-15-minute-chicken-pasta/", "https://www.delish.com/cooking/recipe-ideas/recipes/a58552/general-tso-meatballs-recipe/"];
 
 random.addEventListener('click', () => {
